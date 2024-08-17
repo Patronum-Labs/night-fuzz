48afb654ff60b16442dd31ed09dd785b14204668
YamenMerhi <yamennmerhi@gmail.com>
2024-08-18 00:42:26 +0300

    Set conditional logic

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
+    function setNumber(uint256 _number) public {
+        if (
+            _number ==
+            uint256(keccak256(abi.encodePacked((address(this).code.length))))
+        ) {
+            number = 5;
+        } else {
+            number = _number;
+        }
     }
 }
